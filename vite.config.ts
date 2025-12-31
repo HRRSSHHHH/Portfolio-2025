447a15dc68111110dd63530ab0a4dc8119b4b2aa
HRRSSHHHH <gccontentid@gmail.com>
2025-12-31 20:39:23 +0530

    fix: explicit build base path in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Portfolio-2025/' : '/',
+export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
@@ -15,4 +14,4 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-}))
+})
